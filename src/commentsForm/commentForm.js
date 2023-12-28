eeda6276e774fa524eb87332ef62098815ff91e5
Pranavi Pradhan <pranavipradhan@Pranavis-MacBook-Air.local>
2023-12-28 18:55:08 +0530

    comment section added

---
+++ src/commentsForm/commentForm.js
@@ -0,0 +1,38 @@
+// CommentForm.js
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addComment } from "../Action/actions";
+import "./commentForm.css";
+
+const CommentForm = ({ postId }) => {
+  const dispatch = useDispatch();
+  const [comment, setComment] = useState("");
+  const userName = useSelector((state) => state.userName);
+
+  const handleCommentSubmit = () => {
+    const finalUserName = userName || "User";
+    dispatch(
+      addComment({
+        postId,
+        comment: { userName: finalUserName, text: comment },
+      })
+    );
+    setComment("");
+  };
+
+  return (
+    <div>
+      <textarea
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        class="comment"
+      />
+      <br />
+      <button onClick={handleCommentSubmit} class="button-2">
+        Add Comment
+      </button>
+    </div>
+  );
+};
+
+export default CommentForm;
